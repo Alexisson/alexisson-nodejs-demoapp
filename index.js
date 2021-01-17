4d90411b27d93130e2171281a7de13115899e601
Ivan Shirokov <ivan.a.shirokov@ya.ru>
2021-01-17 22:09:37 +0600

    demoapp v2

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ import http from 'http';
 import puppeteer from 'puppeteer'
 import CORS from './CORS.js';
 import UserModel from './models/User.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 443;
 const User = UserModel(mongoose);
 const app = appSrc(express, bodyParser, fs, 
     crypto, http, CORS, User, 
     mongoose, puppeteer, process.env.LOGIN);
-const PORT = 443;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
 
 
